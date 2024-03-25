c965a32cc7883802a06c2981764e0dc880a06dfb
Vishal Aditya Kirtaniya <vishalkirtaniyaofficial@gmail.com>
2024-03-25 16:12:27 +0530

    added logo-animation

---
+++ src/anyLogo.js
@@ -0,0 +1,229 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// service-card1 logo
+gsap.fromTo(
+  ".logo-animate1",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card1",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card2 logo
+gsap.fromTo(
+  ".logo-animate2",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card2",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card3 logo
+gsap.fromTo(
+  ".logo-animate3",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card3",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card4 logo
+gsap.fromTo(
+  ".logo-animate4",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card4",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card5 logo
+gsap.fromTo(
+  ".logo-animate5",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card5",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card6 logo
+gsap.fromTo(
+  ".logo-animate6",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card6",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card7 logo
+gsap.fromTo(
+  ".logo-animate7",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card7",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card8 logo
+gsap.fromTo(
+  ".logo-animate8",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card8",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card9 logo
+gsap.fromTo(
+  ".logo-animate9",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card9",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card10 logo
+gsap.fromTo(
+  ".logo-animate10",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card10",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card11 logo
+gsap.fromTo(
+  ".logo-animate11",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card11",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
+
+// service-card12 logo
+gsap.fromTo(
+  ".logo-animate12",
+  { x: +50, opacity: 0 },
+  {
+    x: 0, // Start from above
+    opacity: 1,
+    delay: 0.5,
+    duration: 0.3, // Animation duration
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".service-card12",
+      start: "top 80%", // Trigger animation when the logo is 80% from the top
+      end: "top 10%", // Animation ends when the logo reaches 20% from the bottom
+      toggleActions: "play reverse play reverse", // Animation plays when scrolled into view, reverses when scrolled out
+    },
+  }
+);
